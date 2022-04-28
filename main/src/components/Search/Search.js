b0d1d3b1d9f80ae3dce6d0aa6c623494ce135010
Michal <78083264+TenMichal@users.noreply.github.com>
2022-04-28 14:56:58 +0200

    cuisine sites

---
+++ main/src/components/Search/Search.js
@@ -0,0 +1,12 @@
+import React, {useState} from 'react';
+import styled from 'styled-components';
+import {FaSearch} from 'react-icons/fa'
+
+function Search() {
+  return (
+    <div>Search</div>
+    // 1:30
+  )
+}
+
+export default Search
