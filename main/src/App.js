ff2c5a7afa1b8964fa5367d460cf1c52392f3abf
Michal <78083264+TenMichal@users.noreply.github.com>
2022-04-29 10:46:21 +0200

    search input

--- main/src/App.js
+++ main/src/App.js
@@ -1,11 +1,13 @@
 import Pages from './pages/Pages'
 import Category from './components/Category/Category'
 import { BrowserRouter } from 'react-router-dom'
+import Search from './components/Search/Search'
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Search /> 
         <Category />
         <Pages />
       </BrowserRouter>
---
+++ main/src/pages/Searched.js
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from 'react'
+
+function Searched() {
+  const [searchedRecipes, setSearchedRecipes] = useState([]);
+
+  const getSearched = async (name) => {
+    const data = await fetch(
+      `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&query=${name}`
+    )
+    const recipes = await data.json()
+    setSearchedRecipes(recipes.results)
+  }
+
+  return <div>Searched</div>
+}
+
+export default Searched
+
+// 1:41:30
