6bb527d55f0026e7dee47d64fcf410529a311878
Michal <78083264+TenMichal@users.noreply.github.com>
2022-04-30 14:43:48 +0200

    searching method

--- main/src/pages/Searched.js
+++ main/src/pages/Searched.js
@@ -1,7 +1,10 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
+import {useParams} from 'react-router-dom';
+import styled from 'styled-components';
 
 function Searched() {
   const [searchedRecipes, setSearchedRecipes] = useState([]);
+  let params = useParams();
 
   const getSearched = async (name) => {
     const data = await fetch(
@@ -11,9 +14,44 @@ function Searched() {
     setSearchedRecipes(recipes.results)
   }
 
-  return <div>Searched</div>
+  useEffect(() => {
+    getSearched(params.search);
+  }, [params.search]);
+
+  return <div>
+    <Grid>
+      {searchedRecipes.map((item) => {
+        return (
+        <Card key={item.id}>
+          <img src={item.image} alt="" />
+          <h4>{item.title}</h4>
+        </Card>
+        )
+      })}
+    </Grid>
+  </div>
 }
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
+  grid-gap: 3rem;
+`;
+
+const Card = styled.div`
+  img {
+    width: 100%;
+    border-radius: 2rem;
+  }
+  a {
+    text-decoration: none;
+  }
+  h4 {
+    text-align: center;
+    padding: 1rem;
+  }
+`;
 
 export default Searched
 
-// 1:41:30
+// 1:45
